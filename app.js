15a92307e3943e2c93bd6d922f85df025a0b815b
Sumit Chinchane <sumitchinchane@gmail.com>
2016-12-30 15:48:16 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var morgan = require('morgan');
-
-app.use(morgan('dev'));
-
-var usernames = {};
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
+
 io.on('connection', function(socket){
-	//console.log('Connection opened for Chat');
-	
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.emit('updatechat', socket.username, data);
-	});
-	
 
 	socket.on('adduser', function(username){
 		// we store the username in the socket session for this client
 		socket.username = username;
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		//socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
 	});
-
 	socket.on('chat message', function(msg){
-		//var msgS = socket.username + ': ' + msg;
-		//console.log(msgS);
-		console.log('sending message ' + msg)
-    	io.emit('chat message', socket.username + ': ' +  msg);
-	});
-
-	// socket.on('notifyUser', function(user){
- //    	io.emit('notifyUser', user);
- //  	});
-	// socket.on('notifyUser', function(user){
-	// 	console.log('Notifiying User ' +  user);
- //    	io.emit('notifyUser', user);
- //  	});
+    	io.emit('chat message', socket.username + ': ' + msg);
+  	});
 });
 
-http.listen(3001, function(){
-  console.log('listening on *:3000');
+var port = process.env.PORT || 3000;
+
+http.listen(port, function(){
+  console.log('listening on * : ' + port);
 });
